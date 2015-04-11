825bbcba3b04990089cc76b3485b651c22c2115b
Felix Lee <felix.lee135@gmail.com>
2015-04-11 11:48:34 -0400

    Ghosts now move. THROUGH WALLS

---
+++ Unity/Demo/Demo/Assets/Scripts/ClydeMovement.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClydeMovement : MonoBehaviour {
+
+	public float speedMultiplier;
+	
+	private Rigidbody rb;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	void FixedUpdate() {
+		float horizontal = Input.GetAxis ("Clyde Horizontal");
+		float vertical = Input.GetAxis ("Clyde Vertical");
+		
+		Vector3 movement = new Vector3 (horizontal, vertical, 0.0f);
+		transform.Translate (movement * speedMultiplier * Time.deltaTime);
+	}
+}
---
+++ Unity/Demo/Demo/Assets/Scripts/InkyMovement.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InkyMovement : MonoBehaviour {
+
+	public float speedMultiplier;
+	
+	private Rigidbody rb;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	void FixedUpdate() {
+		float horizontal = Input.GetAxis ("Inky Horizontal");
+		float vertical = Input.GetAxis ("Inky Vertical");
+		
+		Vector3 movement = new Vector3 (horizontal, vertical, 0.0f);
+		transform.Translate (movement * speedMultiplier * Time.deltaTime);
+	}
+}
