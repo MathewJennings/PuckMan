46d8c9b2ae2b57005a2bac8e304440d839dcfed2
Mathew Jennings <mathew.percussion@gmail.com>
2015-04-11 15:05:09 -0400

    Removed silly cursor from showing up because of GUI.

--- Unity/Demo/Demo/Assets/Scripts/Menu.cs
+++ Unity/Demo/Demo/Assets/Scripts/Menu.cs
@@ -8,10 +8,7 @@ public class Menu : VRGUI {
 	// Use this for initialization
 	public override void OnVRGUI()
 	{
-		GUI.skin = skin;
-		
-		GUI.Label(new Rect(0f, 0f, 600f, 100f), "Time: " + Time.time);
-		
-		GUI.Label(new Rect(Screen.width/2 - 400, Screen.height - 100, 800, 100), "Press Space For Menu"); 
+		GUI.Label(new Rect(Screen.width/2, Screen.height - 100, 800, 100), "Press Space For Menu"); 
+
 	}
 }
