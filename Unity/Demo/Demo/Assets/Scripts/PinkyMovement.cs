502b8b0ce9e3010c4675c5c03abfbb86d1755a7e
Felix Lee <felix.lee135@gmail.com>
2015-04-11 13:59:11 -0400

    pinky movement work

--- Unity/Demo/Demo/Assets/Scripts/PinkyMovement.cs
+++ Unity/Demo/Demo/Assets/Scripts/PinkyMovement.cs
@@ -24,19 +24,27 @@ public class PinkyMovement : MonoBehaviour {
 		Vector3 movement = new Vector3 (horizontal, vertical, 0.0f);
 		transform.Translate (movement * speedMultiplier * Time.deltaTime);
 
+		if (horizontal != 0 && vertical != 0) {
+			transform.forward = Vector3.Normalize(
+				new Vector3(horizontal, vertical, 0.0f));
+
+			//Vector3 forward = new Vector3(Vector3.forward.x, Vector3.forward.y, Vector3.forward.z);
+			//transform.rotation = Quaternion.LookRotation (forward, Vector3.up);
+		}
+		/*
 		if (horizontal == 1) {
 			if (vertical == 1) {
-				transform.localEulerAngles = new Vector3(0.0f, 135, 0.0f);
+				transform.localRotation = new Vector3(0.0f, 135, 0.0f);
 			} else if (vertical == 0) {
-				transform.localEulerAngles = new Vector3(0.0f, 90, 0.0f);
+				transform.localRotation = new Vector3(0.0f, 90, 0.0f);
 			} else {
-				transform.localEulerAngles = new Vector3(0.0f, 45, 0.0f);
+				transform.localRotation = new Vector3(0.0f, 45, 0.0f);
 			}
 		} else if (horizontal == 0) {
 
 		} else {
 
-		}
+		}*/
 	}
 
 	void OnTriggerEnter(Collider other) {
