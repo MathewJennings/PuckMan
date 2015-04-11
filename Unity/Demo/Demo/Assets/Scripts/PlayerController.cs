c875b29bc90311ecbbf84c431ad34c580c6d94cd
Mathew Jennings <mathew.percussion@gmail.com>
2015-04-11 04:38:32 -0400

    Added control of pacman with WASD and collisions with ghosts and walls.

--- Unity/Demo/Demo/Assets/Scripts/PlayerController.cs
+++ Unity/Demo/Demo/Assets/Scripts/PlayerController.cs
@@ -3,15 +3,29 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
+	public int speedMultiplier;
+	public Vector3 eulerAngleVelocity;
+
+	private Rigidbody rb;
+
 	// Use this for initialization
 	void Start () {
-	
+		rb = GetComponent<Rigidbody> ();
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		if (Input.GetKeyDown("W")) {
-
+	void FixedUpdate () {
+		if (Input.GetKey("w")) {
+			rb.MovePosition(transform.position + transform.up * speedMultiplier * Time.deltaTime);
+		} else if (Input.GetKey("s")) {
+			rb.MovePosition(transform.position + transform.up * speedMultiplier * Time.deltaTime * -1);
+		}
+		if (Input.GetKey("a")) {
+			Quaternion deltaRotation = Quaternion.Euler (eulerAngleVelocity * Time.deltaTime);
+			rb.MoveRotation(rb.rotation * deltaRotation);
+		} else if (Input.GetKey("d")) {
+			Quaternion deltaRotation = Quaternion.Euler (eulerAngleVelocity * Time.deltaTime * -1);
+			rb.MoveRotation(rb.rotation * deltaRotation);
 		}
 	}
 }
