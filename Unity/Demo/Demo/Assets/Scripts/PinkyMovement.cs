2dcc481b27049b959510ba800cec53fb9e24d848
Felix Lee <felix.lee135@gmail.com>
2015-04-11 13:10:45 -0400

    Pinky likes to roll.

--- Unity/Demo/Demo/Assets/Scripts/PinkyMovement.cs
+++ Unity/Demo/Demo/Assets/Scripts/PinkyMovement.cs
@@ -23,6 +23,20 @@ public class PinkyMovement : MonoBehaviour {
 		
 		Vector3 movement = new Vector3 (horizontal, vertical, 0.0f);
 		transform.Translate (movement * speedMultiplier * Time.deltaTime);
+
+		if (horizontal == 1) {
+			if (vertical == 1) {
+				transform.localEulerAngles = new Vector3(0.0f, 135, 0.0f);
+			} else if (vertical == 0) {
+				transform.localEulerAngles = new Vector3(0.0f, 90, 0.0f);
+			} else {
+				transform.localEulerAngles = new Vector3(0.0f, 45, 0.0f);
+			}
+		} else if (horizontal == 0) {
+
+		} else {
+
+		}
 	}
 
 	void OnTriggerEnter(Collider other) {
