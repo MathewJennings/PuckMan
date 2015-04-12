6953ac93f33821ad34c9675ddd3773c430b8b50b
Mathew Jennings <mathew.percussion@gmail.com>
2015-04-12 00:58:45 -0400

    Changed a LOT to make the network thingy work. Hopefully it actually works.

--- Unity/Demo/Demo/Assets/Scripts/NetworkManagerScript.js
+++ Unity/Demo/Demo/Assets/Scripts/NetworkManagerScript.js
@@ -3,10 +3,11 @@ private var gameName = "PacManUncontrollablyGuzzlingHotSauce";
 private var refreshing:boolean; 
 private var hostData: HostData[];
 
-public var cam1: Camera;
-public var cam2: Camera;
-public var cam3: Camera;
-
+public var pacman: Transform;
+public var inky: Transform;
+public var clyde: Transform;
+public var blinky: Transform;
+public var pinky: Transform;
 
 function startServer() {
 	Network.InitializeServer(2, 25001, !Network.HavePublicAddress);
@@ -28,42 +29,35 @@ function Update() {
 	if (!Network.isClient && !Network.isServer) {
 		if(Input.GetKeyDown('=')) {
 			startServer();
-			GameObject.Find("/OVRPlayerController/OVRCameraRig").active = true;
-			cam1.enabled = true;
-     		cam2.enabled = true;
-			GameObject.Find("/Camera").active = false;
-     		cam3.enabled = false;
 		}
 		if(Input.GetKeyDown('0')) {
 			refreshHostLists();
 			if (hostData) {
 				Network.Connect(hostData[0]);
-				GameObject.Find("/OVRPlayerController/OVRCameraRig").active = false;
-				cam1.enabled = false;
-	     		cam2.enabled = false;
-				GameObject.Find("Camera").active = true;
-	     		cam3.enabled = true;
 			}
 		}
 	}
 }
+
 private var playerCount: int = 0;
 function OnPlayerConnected(player: NetworkPlayer) {
 		Debug.Log("Player " + playerCount++ + 
 			      " connected from " + player.ipAddress + 
 	              ":" + player.port);
-		Network.Instantiate(GameObject.Find("OVRPlayerController"), GameObject.Find("OVRPlayerController").transform.position, GameObject.Find("OVRPlayerController").transform.quaternion, 0);
-		Network.Instantiate(GameObject.Find("Inky"), GameObject.Find("Inky").transform.position, GameObject.Find("Inky").transform.quaternion, 0);
-		Network.Instantiate(GameObject.Find("Clyde"), GameObject.Find("Clyde").transform.position, GameObject.Find("Clyde").transform.quaternion, 0);
-		Network.Instantiate(GameObject.Find("Blinky"), GameObject.Find("Blinky").transform.position, GameObject.Find("Blinky").transform.quaternion, 0);
-		Network.Instantiate(GameObject.Find("Pinky"), GameObject.Find("Pinky").transform.position, GameObject.Find("Pinky").transform.quaternion, 0);
+		Network.Instantiate(pacman, new Vector3(0.38, 0.8, -6.26), Quaternion.identity, 0);
+		Network.Instantiate(inky, new Vector3(0.88, 0.8, 3.44), Quaternion.identity, 0);
+		Network.Instantiate(clyde, new Vector3(2.48, 0.8, 2.44), Quaternion.identity, 0);
+		Network.Instantiate(blinky, new Vector3(-1.82, 0.8, 3.44), Quaternion.identity, 0);
+		Network.Instantiate(pinky, new Vector3(-0.22, 0.8, 2.44), Quaternion.identity, 0);
+		
+		GameObject.Find("/OVRPlayerController/OVRCameraRig").SetActive(true);
+		GameObject.Find("/Camera").SetActive(false);
 }
 
+	
 function OnConnectedToServer() {
-		Debug.Log("Connected to server");
-		Destroy(GameObject.Find("OVRPlayerController"));
-		Destroy(GameObject.Find("Inky"));
-		Destroy(GameObject.Find("Clyde"));
-		Destroy(GameObject.Find("Blinky"));
-		Destroy(GameObject.Find("Pinky"));
-	}
+	Debug.Log("Connected to server");
+	// Send local player name to server ...
+	GameObject.Find("/OVRPlayerController/OVRCameraRig").SetActive(false);
+	GameObject.Find("/Camera").SetActive(true);
+}
