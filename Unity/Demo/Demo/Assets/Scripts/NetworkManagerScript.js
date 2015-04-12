e63a85050a399aa755c1c7453a214e366187501c
Mathew Jennings <mathew.percussion@gmail.com>
2015-04-11 23:20:40 -0400

    Added support (maybe?) for networking two computers in order to share their cameras and play as the damn ghosts!

---
+++ Unity/Demo/Demo/Assets/Scripts/NetworkManagerScript.js
@@ -0,0 +1,50 @@
+﻿
+private var gameName = "PacManUncontrollablyGuzzlingHotSauce";
+private var refreshing:boolean; 
+private var hostData: HostData[];
+
+public var cam1: Camera;
+public var cam2: Camera;
+public var cam3: GameObject;
+
+
+function startServer() {
+	Network.InitializeServer(2, 25001, !Network.HavePublicAddress);
+	MasterServer.RegisterHost(gameName, "Tutorial Game Name", "This is a tutorial game");
+}
+
+function refreshHostLists() {
+	MasterServer.RequestHostList(gameName);
+	refreshing = true;
+}
+
+function Update() {
+	if(refreshing) {
+		if (MasterServer.PollHostList().Length > 0) {
+			refreshing = false;
+			hostData = MasterServer.PollHostList();
+		}
+	}
+	if (!Network.isClient && !Network.isServer) {
+		if(Input.GetKeyDown('=')) {
+			startServer();
+			GameObject.Find("/OVRPlayerController/OVRCameraRig").active = true;
+			cam1.enabled = true;
+     		cam2.enabled = true;
+			GameObject.Find("Camera").active = false;
+     		cam3.enabled = false;
+		}
+		if(Input.GetKeyDown('0')) {
+			refreshHostLists();
+			if (hostData) {
+				Network.Connect(hostData[0]);
+			GameObject.Find("/OVRPlayerController/OVRCameraRig").active = false;
+			cam1.enabled = false;
+     		cam2.enabled = false;
+			GameObject.Find("Camera").active = true;
+     		cam3.enabled = true;
+			}
+		}
+	}
+}
+
