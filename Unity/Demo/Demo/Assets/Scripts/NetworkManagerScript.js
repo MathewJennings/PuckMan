220ce175d75157654a83fcdbf0969b250b779c92
Mathew Jennings <mathew.percussion@gmail.com>
2015-04-12 00:08:37 -0400

    This should sync the characters!!!

--- Unity/Demo/Demo/Assets/Scripts/NetworkManagerScript.js
+++ Unity/Demo/Demo/Assets/Scripts/NetworkManagerScript.js
@@ -5,7 +5,7 @@ private var hostData: HostData[];
 
 public var cam1: Camera;
 public var cam2: Camera;
-public var cam3: GameObject;
+public var cam3: Camera;
 
 
 function startServer() {
@@ -31,20 +31,39 @@ function Update() {
 			GameObject.Find("/OVRPlayerController/OVRCameraRig").active = true;
 			cam1.enabled = true;
      		cam2.enabled = true;
-			GameObject.Find("Camera").active = false;
+			GameObject.Find("/Camera").active = false;
      		cam3.enabled = false;
 		}
 		if(Input.GetKeyDown('0')) {
 			refreshHostLists();
 			if (hostData) {
 				Network.Connect(hostData[0]);
-			GameObject.Find("/OVRPlayerController/OVRCameraRig").active = false;
-			cam1.enabled = false;
-     		cam2.enabled = false;
-			GameObject.Find("Camera").active = true;
-     		cam3.enabled = true;
+				GameObject.Find("/OVRPlayerController/OVRCameraRig").active = false;
+				cam1.enabled = false;
+	     		cam2.enabled = false;
+				GameObject.Find("Camera").active = true;
+	     		cam3.enabled = true;
 			}
 		}
 	}
 }
+private var playerCount: int = 0;
+function OnPlayerConnected(player: NetworkPlayer) {
+		Debug.Log("Player " + playerCount++ + 
+			      " connected from " + player.ipAddress + 
+	              ":" + player.port);
+		Network.Instantiate(GameObject.Find("OVRPlayerController"), GameObject.Find("OVRPlayerController").transform.position, GameObject.Find("OVRPlayerController").transform.quaternion, 0);
+		Network.Instantiate(GameObject.Find("Inky"), GameObject.Find("Inky").transform.position, GameObject.Find("Inky").transform.quaternion, 0);
+		Network.Instantiate(GameObject.Find("Clyde"), GameObject.Find("Clyde").transform.position, GameObject.Find("Clyde").transform.quaternion, 0);
+		Network.Instantiate(GameObject.Find("Blinky"), GameObject.Find("Blinky").transform.position, GameObject.Find("Blinky").transform.quaternion, 0);
+		Network.Instantiate(GameObject.Find("Pinky"), GameObject.Find("Pinky").transform.position, GameObject.Find("Pinky").transform.quaternion, 0);
+}
 
+function OnConnectedToServer() {
+		Debug.Log("Connected to server");
+		Destroy(GameObject.Find("OVRPlayerController"));
+		Destroy(GameObject.Find("Inky"));
+		Destroy(GameObject.Find("Clyde"));
+		Destroy(GameObject.Find("Blinky"));
+		Destroy(GameObject.Find("Pinky"));
+	}
