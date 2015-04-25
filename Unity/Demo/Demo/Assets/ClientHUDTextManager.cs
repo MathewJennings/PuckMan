4f74df9967c087ad9e6c5fbea6d1c8de74986dbb
Mathew Jennings <mathew.percussion@gmail.com>
2015-04-24 21:32:21 -0400

    Added text for client to see score, lives, and pellets remaining.

---
+++ Unity/Demo/Demo/Assets/ClientHUDTextManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ClientHUDTextManager : MonoBehaviour {
+
+	public Text scoreText;
+	public Text livesText;
+	public Text pelletText;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		scoreText.text = "TITS1";
+		livesText.text = "TITS2";
+		pelletText.text = "TITS3";
+	}
+}
